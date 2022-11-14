1cc200592f527daa669aae78ab5bd359131a760f
galacticwarrior9 <zareef1@yahoo.com>
2022-11-14 01:00:24 +0000

    Add reward for recruited player and filler pane

--- src/main/java/com/ccnetmc/referrals/Config.kt
+++ src/main/java/com/ccnetmc/referrals/Config.kt
@@ -3,6 +3,7 @@ package com.ccnetmc.referrals
 object Config {
     // Rewards
     var baseRewardPayment = 0.0
+    var recruiteeRewardPayment = 0.0
 
     // Requirements
     var daysSinceFirstJoinLimit = 1000
@@ -12,6 +13,7 @@ object Config {
         val config = plugin.config
 
         baseRewardPayment = config.getDouble("rewards.base-payment")
+        recruiteeRewardPayment = config.getDouble("rewards.recruitee-payment")
 
         daysSinceFirstJoinLimit = config.getInt("requirements.days-since-first-join-limit")
         minimumPlaytimeInHours = config.getInt("requirements.playtime-in-hours")
--- src/main/java/com/ccnetmc/referrals/commands/ReferralsCommand.kt
+++ src/main/java/com/ccnetmc/referrals/commands/ReferralsCommand.kt
@@ -7,7 +7,10 @@ import com.ccnetmc.referrals.requirements.RankRequirement
 import com.ccnetmc.referrals.requirements.StayedInTownRequirement
 import com.github.stefvanschie.inventoryframework.gui.GuiItem
 import com.github.stefvanschie.inventoryframework.gui.type.ChestGui
+import com.github.stefvanschie.inventoryframework.pane.Pane
+import com.github.stefvanschie.inventoryframework.pane.PatternPane
 import com.github.stefvanschie.inventoryframework.pane.StaticPane
+import com.github.stefvanschie.inventoryframework.pane.util.Pattern
 import com.palmergames.bukkit.towny.TownyAPI
 import com.palmergames.bukkit.towny.TownyMessaging
 import com.palmergames.bukkit.towny.`object`.Resident
@@ -73,8 +76,18 @@ class ReferralsCommand(val plugin: Referrals) : TabExecutor {
         val recruitedPlayer = Bukkit.getOfflinePlayer(recruitedResident.name)
 
         val gui = ChestGui(1, "Recruitment Bonus Reward")
+
+        val fillerPattern = Pattern("111010111")
+        val fillerPane = PatternPane(0, 0, 9, 1, Pane.Priority.LOWEST, fillerPattern)
+        fillerPane.setOnClick { it.isCancelled = true }
+
+        val fillerPaneItem = ItemStack(Material.GRAY_STAINED_GLASS_PANE)
+        fillerPaneItem.editMeta { it.displayName(Component.empty()) }
+        fillerPane.bindItem('1', GuiItem(fillerPaneItem))
+        gui.addPane(fillerPane)
+
         val requirementsPane = StaticPane(3, 0, 1, 1)
-        val requirementsItem = ItemStack(Material.PAPER)
+        val requirementsItem = ItemStack(Material.WRITABLE_BOOK)
 
         val requirements = listOf(
             StayedInTownRequirement(recruitedResident),
@@ -112,7 +125,10 @@ class ReferralsCommand(val plugin: Referrals) : TabExecutor {
             val redeemRewardPane = StaticPane(5, 0, 1, 1)
             val redeemRewardItem = if (requirementsSatisfied) ItemStack(Material.LIME_STAINED_GLASS_PANE) else ItemStack(Material.BARRIER)
             redeemRewardItem.editMeta {
-                it.lore(listOf(miniMessage.deserialize("<!i><gray>Reward: <yellow>$${Config.baseRewardPayment.toInt()}")))
+                it.lore(listOf(
+                    miniMessage.deserialize("<!i><gray>•</gray> <white>The <#edc187>town bank</#edc187> will receive: <#eded87>$${Config.baseRewardPayment.toInt()}"),
+                    miniMessage.deserialize("<!i><gray>•</gray> <white><#edc187>${recruitedPlayer.name}</#edc187> will receive: <#eded87>$${Config.recruiteeRewardPayment.toInt()}"))
+                )
                 if (requirementsSatisfied) {
                     it.displayName(miniMessage.deserialize("<#49de23><!i><b>✓</b> Click to redeem reward"))
                 }
@@ -125,8 +141,14 @@ class ReferralsCommand(val plugin: Referrals) : TabExecutor {
                 it.isCancelled = true
                 // Give rewards
                 if (requirementsSatisfied) {
-                    town.account.deposit(Config.baseRewardPayment, "Recruitment bonus.")
+                    town.account.deposit(Config.baseRewardPayment, "Recruitment bonus (mayor)")
+                    recruitedResident.accountOrNull?.deposit(Config.recruiteeRewardPayment, "Recruitment bonus (recruitee)")
+
                     TownyMessaging.sendPrefixedTownMessage(town, "Earned $${Config.baseRewardPayment} in recruitment bonuses from ${recruitedResident.name}!")
+                    if (recruitedPlayer.isOnline) {
+                        (recruitedPlayer as Player).playSound(recruiterPlayer.location, Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 3f, 2f)
+                        recruitedPlayer.sendRichMessage("<green><b>✓</b></green> <#b7f0b6>You have earned <#b6f0ef>$${Config.recruiteeRewardPayment}</#b6f0ef> from the recruitment bonus reward!")
+                    }
                     plugin.logger.info("${recruiterPlayer.name} of the town of ${town.name} earned a recruitment bonus for ${recruitedResident.name}")
 
                     MetaDataUtil.setBoolean(recruitedResident, recruitBonusClaimedField, true, true)
